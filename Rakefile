fa70668d0ae9fd30a47ecb10ef386e2094e850df
John Mettraux <jmettraux@gmail.com>
2009-12-25 16:46:45 +0900

    initial commit

---
+++ Rakefile
@@ -0,0 +1,83 @@
+
+
+require 'lib/rufus/json.rb'
+
+require 'rubygems'
+require 'rake'
+
+
+#
+# CLEAN
+
+require 'rake/clean'
+CLEAN.include('pkg', 'tmp', 'html')
+task :default => [ :clean ]
+
+
+#
+# GEM
+
+require 'jeweler'
+
+Jeweler::Tasks.new do |gem|
+
+  gem.version = Rufus::Json::VERSION
+  gem.name = 'rufus-json'
+
+  gem.summary = 'One interface to various JSON ruby libs (yajl, json, json_pure, json-jruby, active_support). Has a preference for yajl.'
+
+  gem.description = %{
+One interface to various JSON ruby libs (yajl, json, json_pure, json-jruby, active_support). Has a preference for yajl.
+  }
+  gem.email = 'jmettraux@gmail.com'
+  gem.homepage = 'http://github.com/jmettraux/rufus-json/'
+  gem.authors = [ 'John Mettraux' ]
+  gem.rubyforge_project = 'rufus'
+
+  gem.test_file = 'test/test.rb'
+
+  gem.add_development_dependency 'json'
+  gem.add_development_dependency 'yajl-ruby'
+  gem.add_development_dependency 'activesupport'
+  #gem.add_development_dependency 'json_pure'
+  #gem.add_development_dependency 'json-jruby'
+  gem.add_development_dependency 'yard', '>= 0'
+
+  # gemspec spec : http://www.rubygems.org/read/chapter/20
+end
+Jeweler::GemcutterTasks.new
+
+
+#
+# DOC
+
+begin
+
+  require 'yard'
+
+  YARD::Rake::YardocTask.new do |doc|
+    doc.options = [
+      '-o', 'html/rufus-json', '--title',
+      "rufus-json #{Rufus::Json::VERSION}"
+    ]
+  end
+
+rescue LoadError
+
+  task :yard do
+    abort "YARD is not available : sudo gem install yard"
+  end
+end
+
+
+#
+# TO THE WEB
+
+task :upload_website => [ :clean, :yard ] do
+
+  account = 'jmettraux@rubyforge.org'
+  webdir = '/var/www/gforge-projects/rufus'
+
+  sh "rsync -azv -e ssh html/rufus-json #{account}:#{webdir}/"
+end
+
---
+++ test/test.rb
@@ -0,0 +1,133 @@
+
+#
+# testing rufus-json
+#
+# Fri Jul 31 13:05:37 JST 2009
+#
+
+require 'test/unit'
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rufus/json'
+require 'rubygems'
+
+
+class JsonTest < Test::Unit::TestCase
+
+  def setup
+    Rufus::Json.backend = Rufus::Json::NONE
+  end
+  #def teardown
+  #end
+
+  def test_no_backend
+
+    assert_raise RuntimeError do
+      Rufus::Json.decode('nada')
+    end
+  end
+
+  def test_get_backend
+
+    assert_equal :none, Rufus::Json.backend
+
+    require 'json'
+
+    Rufus::Json.detect_backend
+
+    assert_not_equal :none, Rufus::Json.backend
+  end
+
+  def test_set_backend
+
+    require 'json'
+
+    Rufus::Json.backend = :json
+
+    assert_equal :json, Rufus::Json.backend
+  end
+
+  def test_decode_json
+
+    do_test_decode('json', Rufus::Json::JSON)
+  end
+
+  def test_encode_json
+
+    do_test_encode('json', Rufus::Json::JSON)
+  end
+
+  def test_dup_json
+
+    do_test_dup('json', Rufus::Json::JSON)
+  end
+
+  def test_decode_yajl
+
+    do_test_decode('yajl', Rufus::Json::YAJL)
+  end
+
+  def test_encode_yajl
+
+    do_test_encode('yajl', Rufus::Json::YAJL)
+  end
+
+  def test_dup_yajl
+
+    do_test_dup('yajl', Rufus::Json::YAJL)
+  end
+
+  def test_decode_as
+
+    do_test_decode('active_support', Rufus::Json::ACTIVE)
+  end
+
+  def test_encode_as
+
+    do_test_encode('active_support', Rufus::Json::ACTIVE)
+  end
+
+  def test_dup_as
+
+    do_test_dup('active_support', Rufus::Json::ACTIVE)
+  end
+
+  protected
+
+  def do_test_decode (lib, cons)
+
+    require lib
+
+    assert_raise RuntimeError do
+      Rufus::Json.decode('nada')
+    end
+
+    Rufus::Json.backend = cons
+    assert_equal [ 1, 2, 3 ], Rufus::Json.decode("[ 1, 2, 3 ]")
+  end
+
+  def do_test_encode (lib, cons)
+
+    require lib
+
+    assert_raise RuntimeError do
+      Rufus::Json.encode('nada')
+    end
+
+    Rufus::Json.backend = cons
+    assert_equal "[1,2,3]", Rufus::Json.encode([ 1, 2, 3 ])
+  end
+
+  def do_test_dup (lib, cons)
+
+    require lib
+
+    d0 = { 'id' => 'nada' }
+    d1 = { :id => :nada }
+
+    Rufus::Json.backend = cons
+
+    assert_equal({ 'id' => 'nada' }, Rufus::Json.dup(d0))
+    assert_equal({ 'id' => 'nada' }, Rufus::Json.dup(d1))
+  end
+end
+
